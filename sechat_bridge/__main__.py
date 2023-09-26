ac3d5919ffb0fd34ce32bf9da33dc20db04811cc
William Baker <bbrk24@gmail.com>
2023-09-26 10:15:16 -0400

    lots more work

--- sechat_bridge/__main__.py
+++ sechat_bridge/__main__.py
@@ -1,8 +1,9 @@
 import asyncio
 import itertools
 import re
-from typing import Any, Generator, Optional
+from typing import Any, Generator, List, Optional, Tuple, Union
 
+import aiohttp
 import bs4
 import discord
 import sechat
@@ -12,42 +13,111 @@ from .secrets import *
 
 room: Optional[sechat.Room] = None
 
+BADGE_INFOS = [
+    ('badge1', '🥇'),
+    ('badge2', '🥈'),
+    ('badge3', '🥉')
+]
+
 class DiscordClient(discord.Client):
     def __init__(self, intents: discord.Intents):
-        self.channel_id = 771395175595245603
+        self.CHANNEL_ID = 771395175595245603
         self.channel: Optional[discord.abc.GuildChannel | discord.Thread] = None
         super().__init__(intents=intents)
     
     async def on_ready(self):
-        self.channel = await self.fetch_channel(self.channel_id)
+        self.channel = await self.fetch_channel(self.CHANNEL_ID)
 
     async def on_message(self, message: discord.Message):
-        if message.author.id != self.user.id and message.channel.id == self.channel_id and room is not None:
+        if message.author.id != self.user.id and message.channel.id == self.CHANNEL_ID and room is not None:
             await room.send(f'{message.author.display_name}: {message.content}')
 
-html_regex = re.compile(r"<(\w+).*>.*</\1>", re.RegexFlag.DOTALL)
-def se_content_to_str(content: str):
-    if html_regex.match(content):
+HTML_REGEX = re.compile(r"<(\w+).*>.*</\1>", re.RegexFlag.DOTALL)
+async def se_content_to_str(content: str, session: aiohttp.ClientSession) -> Tuple[str, Union[discord.Embed, discord.File, None]]:
+    if HTML_REGEX.match(content):
+        # FIXME: doesn't accept single-line messages that contain HTML
         try:
             soup = bs4.BeautifulSoup(content)
             print('translating HTML to MD:', content)
         except bs4.ParserRejectedMarkup:
-            return content
+            print('Error, not HTML:', content)
+            return content, None
     else:
-        return content
+        return content, None
     
     onebox = soup.find(lambda el: el.has_attr('class') and 'onebox' in el['class'])
     if onebox is not None:
         ob_type = list(filter(lambda c: c.startswith('ob-'), onebox['class']))[0]
         match ob_type:
             case 'ob-image':
-                return soup.find('img').attrs['src']
+                img = soup.find('img')
+                url = img.attrs['src']
+                if url.startswith('//'):
+                    url = 'https:' + url
+                
+                return url, None # FIXME: The below crashes with a null byte?
+                alt = img.attrs['alt'] if img.has_attr('alt') else None
+                
+                async with session.get(url) as response:
+                    data = await response.content.read()
+                    return '', discord.File(
+                        data,
+                        description=alt
+                    )
             case 'ob-youtube' | 'ob-wikipedia' | 'ob-xkcd':
-                return soup.find('a').attrs['href']
+                return soup.find('a').attrs['href'], None
             case 'ob-message':
-                return 'https://chat.stackexchange.com' + soup.find('a', {'class': 'roomname'}).attrs['href']
+                return 'https://chat.stackexchange.com' + soup.find('a', class_='roomname').attrs['href'], None
             case 'ob-post':
-                return 'https:' + soup.find('a').attrs['href']
+                return 'https:' + soup.find('a').attrs['href'], None
+            case 'ob-user':
+                username_a = soup.find('a', class_='ob-user-username')
+                user_name = discord.utils.escape_markdown(username_a.text)
+                pfp_url = soup.find(class_='user-gravatar64').find('img').attrs['src']
+
+                embed = discord.Embed(title=user_name).set_thumbnail(url=pfp_url)
+
+                user_url = 'https:' + username_a.attrs['href']
+                site_icon = username_a.previous_sibling
+                if isinstance(site_icon, bs4.Tag) and site_icon.has_attr('title'):
+                    embed.description = f'[User on {site_icon["title"]} Stack Exchange]({user_url})'
+                else:
+                    embed.description = f'Stack Exchange User: <{user_url}>'
+                
+                reputation = soup.find(class_='reputation-score')
+                if reputation is not None:
+                    embed.add_field(
+                        name='Reputation',
+                        value=reputation.text
+                    )
+                
+                badges: List[str] = []
+                for class_name, icon in BADGE_INFOS:
+                    badge_icon_tag = soup.find(class_=class_name)
+                    if badge_icon_tag is None:
+                        continue
+                    badge_count_tag = badge_icon_tag.next_sibling
+                    if badge_count_tag is None:
+                        continue
+                    assert 'badgecount' in badge_count_tag.attrs['class']
+                    badges.append(f'{icon} {badge_count_tag.text}')
+                if len(badges) > 0:
+                    embed.add_field(
+                        name='Badges',
+                        value=', '.join(badges)
+                    )
+
+                tags = soup.find_all(class_='ob-user-tag')
+                if len(tags) > 0:
+                    embed.add_field(
+                        name='Top Tags',
+                        value='\n'.join(
+                            map(lambda tag: f'\\[{tag.text.strip()}\\] \\({tag.attrs["title"]}\\)', tags)
+                        ),
+                        inline=False
+                    )
+                
+                return ' ', embed
             case _:
                 print('Unknown onebox type', ob_type)
 
@@ -88,10 +158,13 @@ def se_content_to_str(content: str):
                     yield from itertools.chain(*map(translate_tag, tag.children))
                     yield '~~'
                 case _:
+                    if tag.has_attr('class') and 'quote' in tag['class']:
+                        yield '\n>>> '
                     yield from itertools.chain(*map(translate_tag, tag.children))
+            if tag.has_attr('class') and 'partial' in tag['class']:
+                yield '...'
         else:
             yield discord.utils.escape_markdown(tag.text)
-
 
     # holy cow Python, why is this so complicated. Why can I not just have flatmap
     return ''.join(itertools.chain(*map(translate_tag, soup.children)))
@@ -109,13 +182,25 @@ async def main():
 
     discord_client = DiscordClient(intents=intents)
 
-    @room.on(sechat.EventType.MESSAGE)
-    async def on_message(room: sechat.Room, message: sechat.MessageEvent):
-        if message.user_id != sechat_client.userID and discord_client.channel is not None:
-            message_content = se_content_to_str(message.content)
-            await discord_client.channel.send(f'{message.user_name}: {message_content}')
+    async with aiohttp.ClientSession() as session:
+        @room.on(sechat.EventType.MESSAGE)
+        async def on_message(room: sechat.Room, message: sechat.MessageEvent):
+            if message.user_id != sechat_client.userID and discord_client.channel is not None:
+                message_content, embed = await se_content_to_str(message.content, session)
+                if embed is None:
+                    await discord_client.channel.send(f'{message.user_name}: {message_content}')
+                elif isinstance(embed, discord.File):
+                    await discord_client.channel.send(
+                        f'{message.user_name}: {message_content}',
+                        file=embed
+                    )
+                else:
+                    await discord_client.channel.send(
+                        f'{message.user_name}: {message_content}',
+                        embed=embed
+                    )
 
-    await discord_client.start(token)
+        await discord_client.start(token)
 
 if __name__ == '__main__':
     asyncio.run(main())
